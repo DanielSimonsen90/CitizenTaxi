14c67e2f9aadbbbaec4abad50dd8eaff14362b62
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-11-10 10:08:19 +0100

    Create project with mock data

---
+++ Projects/Backend/Common/Entities/User/MockData.cs
@@ -0,0 +1,20 @@
+﻿using Common.Enums;
+
+namespace Common.Entities.User;
+
+internal static class MockData
+{
+    internal static readonly Admin Admin = new("Admin", Role.Admin);
+    internal static readonly Citizen Citizen = new("Citizen", Role.Citizen);
+
+    internal static readonly Login AdminLogin = new("admin", "admin123", Admin);
+    internal static readonly Login CitizenLogin = new("citizen", "citizen123", Citizen);
+
+    internal static readonly Note CitizenNote = new(Citizen, pensioner: true, residence: "Solvej 10, 0000 God by", 
+        CarHeight.High, HelpingUtil.WheelChair, Companion.WithHelper, Follow.Both);
+    internal static readonly List<Booking> CitizenBookings = new()
+    {
+        new(pickup: "Solvej 10, 0000 God by", destination: "Frederikshavn Sygehus", arrival: DateTime.Now.AddHours(2), Citizen),
+        new(pickup: "Frederikshavn Sygehus", destination: "Solvej 10, 0000 God by", arrival: DateTime.Now.AddHours(3).AddMinutes(30), Citizen)
+    };
+}
