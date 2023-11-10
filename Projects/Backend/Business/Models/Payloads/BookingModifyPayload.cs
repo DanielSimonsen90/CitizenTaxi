4c7289bb007fe89ffb23b54f5a4e1117e63f0ddf
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-11-10 12:50:32 +0100

    Create Business project + payload models

---
+++ Projects/Backend/Business/Models/Payloads/BookingModifyPayload.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+namespace Business.Models.Payloads;
+
+public class BookingModifyPayload
+{
+    public Guid CitizenId { get; set; }
+    public string Pickup { get; set; }
+    public string Destination { get; set; }
+    public DateTime Arrival { get; set; }
+}
---
+++ Projects/Backend/Business/Models/Payloads/NoteModifyPayload.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+using Common.Enums;
+
+namespace Business.Models.Payloads;
+
+public class NoteModifyPayload
+{
+    public Guid CitizenId { get; set; }
+    public bool Pensioner { get; set; }
+    public string Residence { get; set; }
+    public CarHeight CarHeight { get; set; }
+    public HelpingUtil HelpingUtil { get; set; }
+    public Companion Companion { get; set; }
+    public Follow Follow { get; set; }
+}
