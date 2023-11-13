5f52be720b09c948c2ff769b0c3647c9f9ab24ed
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-11-13 16:08:46 +0100

    Finish note test

--- Projects/Backend/Tests/DataAccessTests/NotesRepositoryTest.cs
+++ Projects/Backend/Tests/DataAccessTests/NotesRepositoryTest.cs
@@ -1,6 +1,9 @@
 ﻿using Common.DTOs;
 using Common.Entities;
+using Common.Entities.User;
+using Common.Enums;
 using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace DataAccessTests;
 
@@ -11,5 +14,30 @@ internal class NotesRepositoryTest : ABaseRepositoryTest<Note, NoteDTO, NoteRepo
     {
         entity.Pensioner = true;
         return entity;
+    }
+
+    [Test]
+    public void GetNoteFromCitizen_ReturnsNoteAssociatedWithCitizen()
+    {
+        // Arrange
+        var a = TestConstants.TEST_CITIZEN.Clone();
+        var b = TestConstants.TEST_CITIZEN.Clone(Guid.NewGuid());
+        var note = TestConstants.TEST_NOTE.Clone();
+
+        a.Note = note;
+        UnitOfWork.Citizens.Add(a);
+        UnitOfWork.SaveChanges(); // Note should be added to database with citizen
+
+        // Act
+        var resultA = Repository.GetFromCitizen(a);
+        var resultB = Repository.GetFromCitizen(b);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(resultA, Is.Not.Null);
+            Assert.That(resultA!.Id, Is.EqualTo(note.Id));
+            Assert.That(resultB, Is.Null);
+        });
     }
 }
---
+++ Projects/Backend/Tests/DataAccessTests/TestConstants.cs
@@ -0,0 +1,31 @@
+﻿using Common.Entities;
+using Common.Entities.User;
+using Common.Enums;
+using DanhoLibrary.NLayer;
+using System.Text.Json;
+#nullable disable
+
+namespace DataAccessTests;
+
+internal static class TestConstants
+{
+    public const string CITIZEN_NAME = "Test citizen";
+    public const string RESIDENCE = "Solvej 10, 0000 God by";
+
+    public static Citizen TEST_CITIZEN = new (CITIZEN_NAME, Role.Citizen);
+    public static Note TEST_NOTE = new (
+        pensioner: true,
+        residence: RESIDENCE,
+        carHeight: CarHeight.Low,
+        helpingUtil: HelpingUtil.None,
+        companion: Companion.Alone,
+        follow: Follow.No);
+
+    public static T Clone<T>(this T self, Guid? newId = null) where T : BaseEntity<Guid>
+    {
+        T result = JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(self));
+        if (newId is not null) result.Id = (Guid)newId;
+
+        return result;
+    }
+}
