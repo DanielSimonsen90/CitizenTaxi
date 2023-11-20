17c20eae6be46d5fd4883f3cdc3dc7e161e0a647
Daniel Simonsen <danielsimonsen90@gmail.com>
2023-11-20 13:18:04 +0100

    Clean up and documentation

--- Projects/Frontend/src/components/pages/BookTaxi/BookTaxiLayout.tsx
+++ Projects/Frontend/src/components/pages/BookTaxi/BookTaxiLayout.tsx
@@ -11,21 +11,41 @@ import { BookingStepsPayload } from "./BookTaxiTypes";
 import Progress from "./Steps/Progress";
 
 export default function BookTaxi() {
+  // Destructure the bookingId from the URL
   const {
     bookingId,
     ...params
   } = useParams();
+
+  // Navigator function from react-router-dom to navigate to a different page
   const navigate = useNavigate();
+
+  // Get the citizen from the CitizenProvider to use it for assigning the booking to the citizen
   const { citizen } = useCitizen(true);
+
+  // Get the step from the URL, or default to 1 if it's not a number
   const step = params.step ? Number(params.step) : 1;
 
+  // State to keep track of whether or not the form has been submitted
+  // This is to prevent component updates that accidentally switch to the next step
   const [submitted, setSubmitted] = useState(false);
+
+  // State to keep track of the booking payload. 
+  // This is kept in the URL query string to allow the user to refresh the page 
+  // without losing their progress
   const [payload, setPayload] = useStateInQuery<BookingStepsPayload>('booking', {
     id: bookingId,
     citizenId: citizen?.id,
   });
+
+  // Get the data for the current step
   const { title, description, canContinue, canGoBack } = getStepData(step);
+
+  // Function to update the payload state
   const updatePayload = (data: Partial<BookingStepsPayload>) => setPayload(prev => ({ ...prev, ...data }));
+
+  // When the step is submitted, serialize the form data and update the payload state
+  // This will automatically trigger the useEffect hook below to navigate to the next step
   const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = serializeForm<BookingStepsPayload>(e.target as HTMLFormElement);
@@ -33,38 +53,51 @@ export default function BookTaxi() {
     setSubmitted(true);
   };
 
+  // Function to navigate to the next step while keeping the booking payload in the URL
   const navigateToStep = (step: number) => navigate(
-    `${step}?booking=${JSON.stringify(payload)}`
+    `${step}?booking=${JSON.stringify(payload)}&bookingId=${bookingId}`
   );
 
+  // When any of the dependencies update, run the callback
   useUpdateEffect(() => {
+    // If the form hasn't been submitted, don't do anything as this was triggered by internal state changes
     if (!submitted) return;
+
+    // If the user has more steps to complete, navigate to the next step
     if (canContinue) {
       setSubmitted(false);
       return navigateToStep(step + 1);
     } 
+
+    // If the user has completed all steps, make sure the citizenId is set
     if (!payload.citizenId) return updatePayload({ citizenId: citizen?.id });
 
+    // Once the steps are completed and the citizenId is set, send the booking to the API
     (async function sendBooking() {
-      console.log('Sending booking', payload);
       await RequestBookings(payload.citizenId, {
+        // If the payload has an id, use PUT, otherwise use POST
         method: 'id' in payload && payload.id ? 'PUT' : 'POST',
         body: {
           ...payload,
+          // Combine the date and time into a single ISO string
           arrival: new Date(`${payload.date}T${payload.time}Z`).toISOString()
         }
       });
+
+      // Navigate to the citizen page once the booking has been sent
       navigate('/');
     })();
   }, [submitted, payload, canContinue, navigate]);
-
-  useUpdateEffect(() => {
-  }, [citizen?.id]);
 
   return (
     <div className="book-taxi">
       <header>
         <nav>
+          {/*
+            Using the react-router's Link component ultimately renders an <a> tag, 
+            but it will use the react-router's history API to navigate to the page 
+            instead of reloading the page unnecessarily 
+          */}
           <Link to="/">← Annullér bestillingsprocessen</Link>
         </nav>
       </header>
@@ -75,6 +108,7 @@ export default function BookTaxi() {
           <p className="secondary">{description}</p>
 
           <form onSubmit={onFormSubmit}>
+            {/* The Outlet component is from react-router, that will render sub-routes into this layout page */}
             <Outlet />
 
             <footer className="button-container">
